ab97094b2b41bb5d87be1e57d674a40eafd0a5b9
douvy <rjd5146@gmail.com>
2023-03-28 14:54:20 -0400

    added icons to app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;600&display=swap" rel="stylesheet" />
+          <script src="https://kit.fontawesome.com/7f8f63cf7a.js" crossOrigin="anonymous"></script>
         </Head>
         <body>
           <Main />
