f1357840eac54580fe8dd4e32531ca3878ff5787
douvy <rjd5146@gmail.com>
2023-04-02 02:37:01 -0400

    made top part of sidebar scrollable and removed dead links

--- src/components/ConversationLinkList.js
+++ src/components/ConversationLinkList.js
@@ -3,13 +3,15 @@ import ConversationLinkListItem from './ConversationLinkListItem';
 
 function ConversationLinkList({ conversations, activeConversation, selectConversation, session, setCurrentRoute, newConversation }) {
   return (
-    <ul className="pl-3">
-      <a href="#" onClick={newConversation} id="new-chat"><li className="p-2 mt-2 pl-4"><i className="far fa-arrow-up-right fa-lg"></i> New Chat</li></a>
-      <a href="#"><li className="p-2  pl-4 mb-3 mt-1"><img src="/avatar.png" className="w-7 h-7 rounded-full" /><span className="ml-3">{session.user.username}</span></li></a>
-      {conversations.map((conversation, index) => {
-        return <ConversationLinkListItem key={index} index={index} selectConversation={selectConversation} conversation={conversation} isActive={activeConversation.id == conversation.id} setCurrentRoute={setCurrentRoute} />
-      })}
-    </ul>
+    <div className="max-h-[425px] overflow-y-auto">
+      <ul className="pl-3">
+        <a href="#" onClick={newConversation} id="new-chat"><li className="p-2 mt-2 pl-4"><i className="far fa-arrow-up-right fa-lg"></i> New Chat</li></a>
+        <a href="#"><li className="p-2  pl-4 mb-3 mt-1"><img src="/avatar.png" className="w-7 h-7 rounded-full" /><span className="ml-3">{session.user.username}</span></li></a>
+        {conversations.map((conversation, index) => {
+          return <ConversationLinkListItem key={index} index={index} selectConversation={selectConversation} conversation={conversation} isActive={activeConversation.id == conversation.id} setCurrentRoute={setCurrentRoute} />
+        })}
+      </ul>
+    </div>
   );
 };
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -29,16 +29,16 @@ export default function Sidebar({ setConversations, setConversation, handleLogou
         <div>
             <ul className="pl-3">
                 {/* <SidebarItem iconName="trash-can-xmark" itemText="Clear Conversations" onClick={handleClearConversations} /> */}
-                <SidebarItem iconName="brightness" itemText="Light Mode" />
+                {/*<SidebarItem iconName="brightness" itemText="Light Mode" />*/}
                 <SidebarItem iconName="user-hair-mullet" itemText="My Account" />
                 <SidebarItem iconName="arrow-right-from-bracket" itemText="Log Out" onClick={handleLogout} />
-                <SidebarItem iconName="coin" itemText="Crypto" />
-                <SidebarItem iconName="check-square" itemText="Todos" onClick={() => {
+                {/*<SidebarItem iconName="coin" itemText="Crypto" />*/}
+                {/*<SidebarItem iconName="check-square" itemText="Todos" onClick={() => {
                     setCurrentRoute('/tasks');
-                }} />
-                <SidebarItem iconName="check-square" itemText="Features" onClick={() => {
+                }} />*/}
+                {/*<SidebarItem iconName="check-square" itemText="Features" onClick={() => {
                     setCurrentRoute('/features');
-                }} />
+                }} />*/}
                 <SidebarItem iconName="check-square" itemText="Conversations" onClick={() => {
                     setCurrentRoute('/conversations');
                 }} />
