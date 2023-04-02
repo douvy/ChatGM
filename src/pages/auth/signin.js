8b18c673e8ae439c8163743fad4cc95ed76ea0cb
douvy <rjd5146@gmail.com>
2023-04-02 02:05:22 -0400

    fixed sign in page styling

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -60,27 +60,28 @@ function SignInForm() {
     }
 
     return (
-        <div className="container">
-            <div className="formContainer">
+        <div className="container form-container">
+            <div className="mx-auto w-[400px] flex-1 mt-6 md:mt-2">
                 <form onSubmit={(handleSubmit)} className="form">
-                    <h2 className="heading">Sign In</h2>
-                    <div className="formGroup">
-                        <label htmlFor="username" className="label">Username:</label>
-                        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} className="input" />
+                    <h1 className="heading font-display text-title font-medium  uppercase mt-50 mb-30">Sign In</h1>
+                    <div className="formGroup mb-5">
+                        <label htmlFor="username" className="label block font-bold mb-2 text-xs font-semibold uppercase italic text-white">Username:</label>
+                        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} className="input w-full" />
                     </div>
                     <div className="formGroup">
-                        <label htmlFor="password" className="label">Password:</label>
-                        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} className="input" />
+                        <label htmlFor="password" className="label block font-bold mb-2 text-xs font-semibold uppercase italic text-white">Password:</label>
+                        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} className="input w-full" />
                     </div>
-                    <button type="submit" className="submitButton">Sign In</button>
-                    <div className="linksContainer">
-                        <a href="#" onClick={handleSignUp} className="link">Sign Up</a>
-                        <span className="separator">|</span>
-                        <a href="#" onClick={handleForgotPassword} className="link">Forgot Password</a>
+                    <button type="submit" className="submitButton font-semibold w-full mt-5 mb-8 h-11">Sign In</button>
+                    <div className="linksContainer italic">
+                        <a href="#" onClick={handleSignUp} className="link mr-2">Sign Up</a>
+                        <span className="separator mr-2">|</span>
+                        <a href="#" onClick={handleForgotPassword} className="link ">Forgot Password</a>
                     </div>
                 </form>
             </div>
         </div>
+
     );
 }
 
