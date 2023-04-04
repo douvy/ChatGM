2baa0d37fcadf069e85dfbb755d42164dc40acc2
Josh Kornreich <joshkornreich@Ignition-Mac.localdomain>
2023-04-04 19:57:22 -0400

    removed unneeded generate-types file

--- generate-types.ts
+++
@@ -1,9 +0,0 @@
-// generate-types.ts
-
-import { generateTypeGraphQLTypes } from "@generated/typegraphql-prisma";
-
-generateTypeGraphQLTypes({
-  schemaPath: "./prisma/schema.prisma",
-  outputDirPath: "./generated",
-});
-
