a7a13a8e1c6a3d8d9fb536b67afd006cab899523
Josh Kornreich <joshkornreich@Ignition-Mac.localdomain>
2023-04-04 21:33:31 -0400

    .......

--- next.config.js
+++ next.config.js
@@ -7,11 +7,6 @@ const removeImports = require("next-remove-imports")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = removeImports(withPlugins(
