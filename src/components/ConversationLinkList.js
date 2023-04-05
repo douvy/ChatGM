a3ff8f38d39db92eabec904f21dc306a6520e9c0
douvy <rjd5146@gmail.com>
2023-04-05 16:56:42 -0400

    adjusted top part of sidebars max height

--- src/components/ConversationLinkList.js
+++ src/components/ConversationLinkList.js
@@ -7,7 +7,7 @@ function ConversationLinkList({ conversations, activeConversation, selectConvers
         <a href="#" onClick={newConversation} id="new-chat"><li className="p-2 mt-2 pl-4"><i className="far fa-arrow-up-right fa-lg"></i> New Chat</li></a>
         <a href="#"><li className="p-2  pl-4 mb-3 mt-1"><img src="/avatar.png" className="w-7 h-7 rounded-full" /><span className="ml-3">{session ? session.user.username : ''}</span></li></a>
       </ul>
-      <div className="max-h-[315px] overflow-y-auto">
+      <div className="max-h-[280px] overflow-y-auto">
         <ul className="pl-3">
           {conversations.map((conversation, index) => {
             return <ConversationLinkListItem key={index} index={index} selectConversation={selectConversation} conversation={conversation} isActive={activeConversation.id == conversation.id} />
