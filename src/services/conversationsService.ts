8dcb5b57b9cbbee653bd99d6e43df7cabefcbd38
Josh Kornreich <joshkornreich@Ignition-Mac.localdomain>
2023-04-04 21:00:23 -0400

    more type fixes

--- src/services/conversationsService.ts
+++ src/services/conversationsService.ts
@@ -4,13 +4,13 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const conversationsService = {
-  async create(data) {
+  async create(data: any) {
     return await prisma.conversation.create({ data });
   },
   async get(id: number) {
     return await prisma.conversation.findUnique({ where: { id } });
   },
-  async update(id: number, data) {
+  async update(id: number, data: any) {
     return await prisma.conversation.update({ where: { id }, data });
   },
   async delete(id: number) {
