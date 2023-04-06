8041ddcf06d510044c9d8759c6b6aefd6dfb0b30
douvy <rjd5146@gmail.com>
2023-04-06 16:14:26 -0400

    improved page header UI

--- src/components/SavedMessages.js
+++ src/components/SavedMessages.js
@@ -6,8 +6,8 @@ function SavedMessages({ starredMessages, setStarredMessages, role }) {
     const scrollContainer = useRef(null);
 
     return (
-        <div className="container mx-auto max-w-[760px] mt-3 md:mt-8" id="saved">
-            <h1 class="hidden text-title font-medium uppercase mb-10 text-white tracking-wide md:block">Saved</h1>
+        <div className="container mx-auto max-w-[760px] mt-3 md:mt-5" id="saved">
+            <h1 class="hidden text-title font-medium uppercase mb-5 text-white tracking-wide md:block">Saved</h1>
             <div className="overflow-y-auto" id="messages-box" ref={scrollContainer}>
                 {starredMessages.filter((message) => {
                     console.log(message);
