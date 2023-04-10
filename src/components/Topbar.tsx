1f946281f0a401067a83abab248a921705ffe8c4
douvy <rjd5146@gmail.com>
2023-04-10 17:52:38 -0400

    added notification and settings dropdown to top nav
    still a bug where it doesn't appear properly on the new chat screen

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -9,29 +9,117 @@ interface TopbarProps {
 }
 
 const Topbar: React.FC<TopbarProps> = ({ conversation, userInfo, addSystemMessage }) => {
-    const [isMembersExpanded, setIsMembersExpanded] = useState(true);
-    const removeParticipantMutation = trpc.conversations.removeParticipant.useMutation();
+  const [isMembersExpanded, setIsMembersExpanded] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const removeParticipantMutation = trpc.conversations.removeParticipant.useMutation();
+
+  const [isBellDropdownOpen, setIsBellDropdownOpen] = useState(false);
+  const [isAvatarDropdownOpen, setIsAvatarDropdownOpen] = useState(false);
 
     return (
         <>
-            <header className="flex items-center justify-between px-4 py-2 relative" id="top-nav">
-                {/* Members dropdown title */}
-                {(conversation?.participants?.length || 0) > 1 &&
-                    <div className="flex items-center space-x-2">
-                        <div
-                            className="flex items-center cursor-pointer w-190"
-                            onClick={() => setIsMembersExpanded(!isMembersExpanded)}
-                        >
-                            <i className={`fa-solid mr-2 w-3 ${isMembersExpanded ? 'fa-arrow-down' : 'fa-arrow-right'} ml-2`}></i>
-                            <span>{`${conversation.participants?.length} members`}</span>
-                        </div>
+        <header className="flex items-center justify-between px-4 py-2 relative" id="top-nav">
+          {/* Members dropdown title */}
+          {(conversation?.participants?.length || 0) > 1 &&
+            <div className="flex items-center space-x-2">
+              <div
+                className="flex items-center cursor-pointer w-190"
+                onClick={() => setIsMembersExpanded(!isMembersExpanded)}
+              >
+                <i className={`fa-solid mr-2 w-3 ${isMembersExpanded ? 'fa-arrow-down' : 'fa-arrow-right'} ml-2`}></i>
+                <span>{`${conversation.participants?.length} members`}</span>
+              </div>
+            </div>
+          }
+          <h1 className="text-xl font-semibold">&nbsp;</h1>
+          <nav className="space-x-4 relative flex z-10">
+            <button
+              className="focus:outline-none"
+              aria-expanded={isBellDropdownOpen}
+              onClick={() => {
+                setIsBellDropdownOpen(!isBellDropdownOpen);
+                setIsAvatarDropdownOpen(false); // Close avatar dropdown
+              }}
+            >
+              <i className="fa-regular fa-bell fa-lg mr-2 "></i>
+            </button>
+            {isBellDropdownOpen && (
+              <div className="absolute right-0 mt-8 w-48 bg-dark border-gray-light rounded-md shadow-lg z-100 dropdown-container bell">
+                <ul className="py-1 text-base leading-6 text-offwhite">
+                <li
+                  aria-label="Notification"
+                  className="flex justify-between items-center px-4 py-2 hover-dark cursor-pointer -mx-0 -mt-1 -mb-1"
+                >
+                  <div className="flex items-center">
+                    <div className="w-10 h-10 rounded-full overflow-hidden mr-3">
+                     <img src={userInfo.avatarSource || "/avatar.png"} className="rounded-full w-10 h-10" />
                     </div>
-                }
-                <h1 className="text-xl font-semibold">&nbsp;</h1>
-                <nav className="space-x-4">
-                    <a href="#" className="hover:text-blue-300"></a>
-                </nav>
-            </header>
+                    <div className="flex flex-col">
+                      <p className="text-sm text-offwhite">
+                        <span>{userInfo?.username}</span> added Josh to new <strong>group conversation</strong>
+                      </p>
+                      <div className="text-xs text-gray-400 mt-0.5">
+                        <span>Funky Chat Frenzy</span>
+                      </div>
+                      <p className="text-xs text-gray-500 mt-0.5">11 hours ago</p>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      aria-label="Mark as read"
+                      className="focus:outline-none"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        className="text-gray-500"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M12 6.5a5.5 5.5 0 110 11 5.5 5.5 0 010-11zm0 1a4.5 4.5 0 100 9 4.5 4.5 0 000-9zm0 2.5a2 2 0 110 4 2 2 0 010-4z"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </li>
+                </ul>
+              </div>
+            )}
+
+            <button
+              className="focus:outline-none"
+              aria-expanded={isAvatarDropdownOpen}
+              onClick={() => {
+                setIsAvatarDropdownOpen(!isAvatarDropdownOpen);
+                setIsBellDropdownOpen(false); // Close bell dropdown
+              }}
+            >
+              <img src={userInfo.avatarSource || "/avatar.png"} className="rounded-full w-6 h-6" />
+            </button>
+            {isAvatarDropdownOpen && (
+              <div className="absolute right-0 mt-8 w-48 bg-dark border-gray-light rounded-md shadow-lg z-100 dropdown-container">
+                <ul className="py-1 text-base leading-6 text-offwhite">
+                  <li className="flex flex-col items-start px-4 py-2 hover-dark cursor-pointer text-sm -mx-0 -mt-1 -mb-1">
+                    <div className="flex items-center w-full">
+                      <div className="w-11 h-11 rounded-full overflow-hidden">
+                        <img src={userInfo.avatarSource || "/avatar.png"} className="rounded-full" />
+                      </div>
+                      <div className="ml-1">
+                        <span className="ml-3">{userInfo?.username}</span>
+                      </div>
+                    </div>
+                    <div className="flex items-center w-full mt-2">
+                      <i className="fa-regular fa-gear"></i>
+                      <span className="ml-2">Settings</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </nav>
+        </header>
             {/* Members dropdown content */}
             {(conversation?.participants?.length || 0) > 1 && isMembersExpanded && (
                 <div className="absolute mt-14 pl-4" id="members">
