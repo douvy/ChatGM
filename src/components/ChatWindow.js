f84753723cbeec32da21dcca774d6b1e083f8349
jbenjaminkr <jbenjaminkr@gmail.com>
2023-04-11 19:02:07 -0400

    reverted chatwindow

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -5,7 +5,6 @@ import { subscribeToChannel } from '../lib/ably';
 import pusher from '../lib/pusher';
 import { client } from '../trpc/client';
 import { trpc } from '../utils/trpc';
-import MentionPopover from './MentionPopover'; // Import the MentionPopover component
 
 function ChatWindow({
   conversationId,
@@ -28,14 +27,63 @@ function ChatWindow({
     trpc.conversations.updateMessages.useMutation();
   const messageCount = useRef(conversation.messages.length);
 
-  // New state and ref for the MentionPopover
-  const [isMentionOpen, setIsMentionOpen] = useState(false);
-  const cursorPositionRef = useRef(null);
+  pusher.connection.bind('connected', async () => {
+    setSocketId(pusher.connection.socket_id);
+    subscribeToChannel();
+  });
 
-  // Existing pusher subscription code...
-  // Existing useEffect for pusher subscription...
+  pusher.connection.bind('error', error => {
+    console.log('Pusher subscription failed:', error);
+  });
 
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const subscribeToChannel = async () => {
+    if (typeof conversationId === 'undefined') {
+      return;
+    }
+    if (channelRef.current) {
+      channelRef.current.unsubscribe();
+    }
+
+    let channelName = `private-conversation-${conversationId}`;
+    const auth = await client.pusher.authenticate.query({
+      socketId: pusher.connection.socket_id,
+      channelName: channelName
+    });
+    channelRef.current = pusher.subscribe(channelName, auth);
+    channelRef.current.bind('new-conversation', function (data) {
+      setConversation(data.conversation);
+    });
+    channelRef.current.bind('new-message', function (data) {
+      if (data.conversationId != conversation.id) {
+        return;
+      }
+      setConversation({
+        ...conversation,
+        messages: [...conversation.messages, data.message]
+      });
+    });
+
+    channelRef.current.bind('client-is-typing', function (newMessage) {
+      console.log('someone is typing!', newMessage);
+      if (!submitLocket) {
+        setSubmitLock(true);
+        console.log('newMessage', newMessage);
+        if (newMessage.content) {
+          setConversation({
+            ...conversation,
+            messages: [...conversation.messages, newMessage]
+          });
+        } else {
+          setConversation(conversation);
+        }
+      }
+      // setConversation(data.conversation);
+    });
+  };
+
+  useEffect(() => {
+    subscribeToChannel();
+  }, [conversationId]);
 
   function handleKeyDown(event) {
     event.stopPropagation();
@@ -44,21 +92,53 @@ function ChatWindow({
       sendMessage();
     } else {
       if (channelRef) {
+        console.log('client is typing', channelRef.current);
         channelRef.current?.trigger('client-is-typing', {
           ...newMessage,
           inProgress: true
         });
       }
-      if (event.key === '@') {
-        setIsMentionOpen(true);
-        cursorPositionRef.current = event.target.selectionStart;
-      } else {
-        setIsMentionOpen(false);
-      }
     }
   }
 
-  // Existing functions...
+  async function updateConversation(
+    messageIndex,
+    updatedMessage,
+    isEdit = false
+  ) {
+    if (conversation.messages[messageIndex].starred != updatedMessage.starred) {
+      if (updatedMessage.starred) {
+        setStarredMessages([...starredMessages, updatedMessage]);
+      } else {
+        setStarredMessages(
+          starredMessages.filter(t => t.id !== updatedMessage.id)
+        );
+      }
+    }
+    conversation.messages[messageIndex] = updatedMessage;
+    const updatedMessages = isEdit
+      ? conversation.messages.filter((message, index) => {
+          return index <= messageIndex;
+        })
+      : [...conversation.messages];
+    console.log(updatedMessages, updatedMessages.length);
+    let updatedConversation = {
+      ...conversation,
+      messages: updatedMessages
+    };
+    setConversation(updatedConversation);
+    if (isEdit) {
+      const updated = updateConversationMutation.mutate(updatedConversation);
+      console.log(updated);
+      updatedConversation = await client.openai.query.mutate(
+        updatedConversation
+      );
+      setConversation({
+        ...updatedConversation,
+        messages: updatedConversation.messages
+      });
+    }
+  }
 
   let messageEnd = null;
   useEffect(() => {
@@ -67,6 +147,7 @@ function ChatWindow({
     }
     messageCount.current = conversation.messages.length;
   }, [conversation]);
+
   if (!conversation.messages) {
     return <></>;
   }
@@ -77,7 +158,36 @@ function ChatWindow({
         id='messages-box'
         ref={scrollContainer}
       >
-        {/* Existing message rendering code... */}
+        {conversation.messages.map((message, index) => {
+          return (
+            <ChatMessage
+              key={conversation.id + index}
+              index={index}
+              message={message}
+              avatarSource={`/${message.avatarSource}`}
+              sender={
+                message.role == 'user'
+                  ? message.sender || userInfo
+                  : 'ChatGPT-3.5'
+              }
+              received={true}
+              updateState={updateConversation}
+              setConversation={setConversation}
+              referencedMessage={referencedMessage}
+              onClick={() => {
+                setReferencedMessage(
+                  message.id == referencedMessage?.id ? null : message
+                );
+              }}
+              userInfo={userInfo}
+            />
+          );
+        })}
+        <div
+          ref={element => {
+            messageEnd = element;
+          }}
+        ></div>
       </div>
       <form className='flex items-end max-w-[760px] mb-3' id='chat-form'>
         <AutoExpandTextarea
@@ -87,21 +197,18 @@ function ChatWindow({
           placeholder='Type your message here...'
           className='w-full p-2 mr-2 bg-dark'
           conversationId={conversationId}
+          //   autoFocus={true}
         />
-        {/* Send button */}
+        <span className='button-container'>
+          <button
+            type='button'
+            onClick={sendMessage}
+            className='font-semibold uppercase p-2'
+          >
+            Send
+          </button>
+        </span>
       </form>
-
-      {/* Conditionally render MentionPopover */}
-      {isMentionOpen && (
-        <MentionPopover
-          onSelect={(username) => {
-            const textBeforeCursor = newMessage.content.slice(0, cursorPositionRef.current);
-            const textAfterCursor = newMessage.content.slice(cursorPositionRef.current);
-            updateMessageValue(textBeforeCursor + username + textAfterCursor);
-            setIsMentionOpen(false);
-          }}
-        />
-      )}
     </div>
   );
 }
