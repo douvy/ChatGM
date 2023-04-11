1268f2a9555c52731ee2b6faafd289024ff783e1
jbenjaminkr <jbenjaminkr@gmail.com>
2023-04-11 09:17:04 -0400

    hopefully sped up rendering

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,77 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { client } from '../trpc/client';
 import { NextPage } from 'next';
 import { Conversation, Message, Feature } from '../interfaces';
-
-
-interface C {
-  key?: string;
-}
+import useSWR from 'swr';
 
 interface PageProps {
-  session: any,
-  conversations: Conversation[],
-  starredMessages: Message[],
-  features?: Feature[],
-  tasks?: any[],
-  userInfo: any,
-  activeTask: any,
-  c: any,
+  session: any;
+  conversations: Conversation[];
+  starredMessages: Message[];
+  features?: Feature[];
+  tasks?: any[];
+  userInfo: any;
+  activeTask: any;
+  c: any;
 }
 
 const DataFetcher: NextPage<any> = ({ children, Component, ...props }) => {
+  const getData = client.users.getInitialPageData.query;
+  const { data, error } = useSWR('users.getInitialPageData', () => getData(), {
+    revalidateOnFocus: false
+  });
 
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [data, setData] = useState<any>(null);
 
-  const [c, setC] = React.useState<C>({});
+  // const res = useSession();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     console.log(res);
+  //     if (res.status != 'authenticated') {
+  //       return;
+  //     }
+  //     const _ = await client.users.getInitialPageData.query({
+  //       session: res.data
+  //     });
+  //     console.log(_);
+  //     setIsLoading(false);
+  //     setData(_);
+  //   };
 
-  useEffect(() => {
-    const handleKeyDown = (event: any) => {
-      console.log("Key pressed:", event.key, typeof event.key);
-      if (typeof event.key == "string") {
-        setC({
-          key: event.key
-        });
-      }
-    };
+  //   fetchData();
+  // }, [res]);
 
-    document.addEventListener("keydown", handleKeyDown);
+  // if (isLoading) {
+  //   return <div></div>;
+  // }
 
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div></div>;
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<any>(null);
-
-  const res = useSession();
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log(res);
-      if (res.status != "authenticated") {
-        return;
-      }
-      const _ = await client.users.getInitialPageData.query({
-        session: res.data,
-      });
-      console.log(_);
-      setIsLoading(false);
-      setData(_);
-    }
-
-    fetchData();
-  }, [res]);
-
-  if (isLoading) {
-    return <div></div>;
-  }
-
-  const mergedProps = { ...props, ...data, c }
+  const mergedProps = { ...props, ...data };
   console.log(mergedProps);
 
   return <>{children(mergedProps)}</>;
-}
-
+};
 
 export default DataFetcher;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@/styles/globals.css';
 import type { AppProps, AppType } from 'next/app';
 import Head from 'next/head';
-import { SessionProvider } from "next-auth/react"
-import React, { useEffect, useState } from 'react'
+import { SessionProvider } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { trpc } from '../utils/trpc';
 import { client } from '../trpc/client';
@@ -11,29 +11,55 @@ import DataFetcher from '../components/DataFetcher';
 interface C {
   key?: string;
 }
+
 const App: AppType = ({ Component, pageProps }: AppProps) => {
+  const [c, setC] = React.useState<C>({});
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      console.log('Key pressed:', event.key, typeof event.key);
+      if (typeof event.key == 'string') {
+        setC({
+          key: event.key
+        });
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>ChatGM</title>
-        <meta name="description" content="a clean, visually appealing interface for ChatGPT" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name='description'
+          content='a clean, visually appealing interface for ChatGPT'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        />
+        <link rel='icon' href='/favicon.ico' />
         {/* <link rel="stylesheet" href="/fontawesome.min.css" /> */}
       </Head>
       {/* <Layout> */}
       <SessionProvider>
-        <DataFetcher Component={Component} {...pageProps}>
+        <DataFetcher Component={Component} {...pageProps} c={c}>
           {/* <Component /> */}
           {(mergedProps: any) => {
-            return <Component {...mergedProps} />
+            return <Component {...mergedProps} />;
           }}
         </DataFetcher>
       </SessionProvider>
       {/* </Layout> */}
     </>
   );
-}
+};
 
 // export default App;
 export default trpc.withTRPC(App);
