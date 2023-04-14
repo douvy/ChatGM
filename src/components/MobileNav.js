c8e571d0c34d98abfa9ca452300303f31363b7d0
douvy <rjd5146@gmail.com>
2023-04-14 12:05:03 -0400

    refactored mobile hamburger icon positioning and improved mobile padding

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -2,7 +2,7 @@ export default function Layout(props) {
   return (
     <div className='fixed top-0 left-0 z-50 flex items-center justify-start w-full p-2 pr-3 lg:hidden'>
       <button className='text-red hover:text-red-500'>
-      <i class="fa-sharp fa-solid fa-bars fa-lg mt-3.5"></i>
+      <i class="fa-sharp fa-solid fa-bars fa-lg mt-3.5 ml-3"></i>
       </button>
     </div>
   );
