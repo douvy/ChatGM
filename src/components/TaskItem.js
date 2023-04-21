88761b65ac9cc9faf76bdd8d583f83834b0ec32b
jbenjaminkr <jbenjaminkr@gmail.com>
2023-04-20 23:34:42 -0400

    fixed up some task linking etc

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -11,7 +11,8 @@ function TaskItem({
   setUserInfo,
   setActiveTask,
   setActiveTaskIndex,
-  completed
+  completed,
+  toggleCompletion
 }) {
   if (task.id == userInfo.activeTaskId) {
     setActiveTaskIndex(index);
@@ -45,9 +46,9 @@ function TaskItem({
   // Add isChecked state
   const [isChecked, setIsChecked] = useState(false);
 
-useEffect(() => {
-  setIsChecked(completed);
-}, [completed]);
+  useEffect(() => {
+    setIsChecked(completed);
+  }, [completed]);
 
   // Update isChecked state when completed prop changes
   useEffect(() => {
@@ -66,62 +67,65 @@ useEffect(() => {
           task.id == userInfo.activeTaskId ? 'bg-dark-blue' : ''
         }`}
       >
-        <div className="w-6 h-6 rounded-full absolute left-4 top-0 flex items-center justify-center p-1">
+        <div className='w-6 h-6 rounded-full absolute left-4 top-0 flex items-center justify-center p-1'>
           <button
-            type="button"
+            type='button'
             className={`w-full h-full rounded-full task_checkbox priority_1 border border-gray-300 mt-2 ${
-              isChecked ? 'bg-green' : ''
+              task.labels.includes('completed') ? 'bg-green' : ''
             }`}
-            role="checkbox"
-            aria-checked={isChecked}
-            aria-label=""
-            onClick={() => {
-              setIsChecked(!isChecked);
+            role='checkbox'
+            aria-checked={task.labels.includes('completed')}
+            aria-label=''
+            onClick={e => {
+              toggleCompletion(task, e);
             }}
           >
-            <div className="w-full h-full rounded-full flex items-center justify-center">
+            <div className='w-full h-full rounded-full flex items-center justify-center'>
               <svg
-                width="24"
-                height="24"
-                fill="currentColor"
-                viewBox="0 0 24 24"
+                width='24'
+                height='24'
+                fill='currentColor'
+                viewBox='0 0 24 24'
                 className={`w-3 h-3 check ${
                   isChecked ? 'opacity-100' : 'opacity-0'
                 } transition-opacity duration-200`}
               >
-                <path d="M11.23 13.7l-2.15-2a.55.55 0 0 0-.74-.01l.03-.03a.46.46 0 0 0 0 .68L11.24 15l5.4-5.01a.45.45 0 0 0 0-.68l.02.03a.55.55 0 0 0-.73 0l-4.7 4.35z"></path>
+                <path d='M11.23 13.7l-2.15-2a.55.55 0 0 0-.74-.01l.03-.03a.46.46 0 0 0 0 .68L11.24 15l5.4-5.01a.45.45 0 0 0 0-.68l.02.03a.55.55 0 0 0-.73 0l-4.7 4.35z'></path>
               </svg>
             </div>
           </button>
         </div>
-        <div className={`flex items-center pl-16 ${isChecked ? 'line-through' : ''}`}>
+        <div
+          className={`flex items-center pl-16 ${
+            isChecked ? 'line-through' : ''
+          }`}
+        >
           <br />
           <p className='text-xs inline-block absolute top-0 right-4 timestamp'>
-          <span className='message-direction'>
-  {''}
-  <i
-    className={`fa-regular fa-arrow-down-left fa-lg ml-1 mr-3 mt-2`}
-  ></i>
-</span>{' '}
-{currentTimestamp}
-<i
-  className={`fa-star ${
-    false ? 'fa-solid' : 'fa-regular'
-  } ml-2 cursor-pointer`}
-  onClick={() => {}}
-></i>
-{true ? (
-  <i className='fa-solid  w-5 h-5 ml-2'></i>
-) : (
-  <i
-    className={`fa-light fa-copy w-5 h-5 ml-2 cursor-pointer`}
-    onClick={() => {
-      // copyToClipboard(task.content);
-    }}
-  ></i>
-)}
-</p>
-
+            <span className='message-direction'>
+              {''}
+              <i
+                className={`fa-regular fa-arrow-down-left fa-lg ml-1 mr-3 mt-2`}
+              ></i>
+            </span>{' '}
+            {currentTimestamp}
+            <i
+              className={`fa-star ${
+                false ? 'fa-solid' : 'fa-regular'
+              } ml-2 cursor-pointer`}
+              onClick={() => {}}
+            ></i>
+            {true ? (
+              <i className='fa-solid  w-5 h-5 ml-2'></i>
+            ) : (
+              <i
+                className={`fa-light fa-copy w-5 h-5 ml-2 cursor-pointer`}
+                onClick={() => {
+                  // copyToClipboard(task.content);
+                }}
+              ></i>
+            )}
+          </p>
 
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
@@ -136,4 +140,3 @@ useEffect(() => {
 }
 
 export default TaskItem;
-
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,6 +6,7 @@ import Pusher from 'pusher-js';
 import { client } from '../trpc/client';
 import { trpc } from '../utils/trpc';
 import TaskItem from './TaskItem';
+import TaskBlock from './TaskBlock';
 import NewTask from './NewTask';
 import ProjectListItem from './ProjectListItem';
 import { TodoistApi } from '@doist/todoist-api-typescript';
@@ -258,6 +259,7 @@ function Tasks({
   //     return <></>
   // }
   let onlyShowOnHover = true;
+  let someTaskActive = !!userInfo.activeTaskId;
   return (
     <div className='mx-auto h-full p-4 pt-0'>
       <div className='overflow-y-auto' ref={scrollContainer}>
@@ -289,274 +291,24 @@ function Tasks({
               let showIcons = editingTask?.id != task.id;
               let isActive = task.id == userInfo.activeTaskId;
               return (
-                <div
-                  key={task.id}
-                  className={`relative aspect-w-1 aspect-h-1 shadow-gray-500
-                  ${pointMap[task.pointValue]}`}
-                  style={{ aspectRatio: '1 / 1' }}
-                >
-                  <div
-                    className={`group bg-dark border h-5/6 border-gray-700 cursor-pointer hover-gray rounded   
-                  ${task.id == userInfo.activeTaskId ? '!bg-blue-950' : ''}
-                  ${task.labels.includes('completed') ? 'bg-green' : ''}
-                  ${task.labels.includes('in-progress') ? '!bg-orange-800' : ''}
-                  `}
-                    onClick={() => activateTask(task, index)}
-                  >
-                    <div
-                      className={`group flex h-full flex-col justify-center gap-4 ${
-                        editingTask?.id == task.id ? '' : 'p-3'
-                      }`}
-                    >
-                      <div className={`${editingTask?.id != task.id ? "blur-on-hover" : ""} w-full h-5/6 overflow-hidden flex flex-col justify-center`}>
-                      {editingTask?.id != task.id ? (
-                        <span className="multi-line-truncate w-full inline-block">{task.content || '|' + task.name + '|'}</span>
-                      ) : (
-                        <textarea
-                          value={editingTask.content}
-                          placeholder={editingTask.content}
-                          onChange={e => (
-                            e.stopPropagation(),
-                            setEditingTask({
-                              ...editingTask,
-                              content: e.target.value
-                            })
-                          )}
-                          onKeyDown={e => {
-                            e.stopPropagation();
-                            if (e.key === 'Escape') {
-                              setEditingTask(null);
-                            }
-                            if (e.key === 'Enter' && !e.shiftKey) {
-                              e.preventDefault();
-                              if (submitTaskLock) {
-                                return;
-                              }
-                              api
-                                .updateTask(task.id, {
-                                  content: editingTask.content
-                                })
-                                .then(updatedTask => {
-                                  setTasks(
-                                    tasks.map(t =>
-                                      t.id != task.id ? t : editingTask
-                                    )
-                                  );
-                                  client.tasks.updateWhere.mutate({
-                                    where: {
-                                      id: editingTask.id
-                                    },
-                                    data: {
-                                      name: editingTask.content
-                                    }
-                                  });
-                                  setEditingTask(null);
-                                  setSubmitTaskLock(false);
-                                })
-                                .catch(err => console.log(err));
-
-                              // create an extension in our database
-                            }
-                          }}
-                          className='w-full h-full p-2 mr-2 bg-dark text-sm p-0 m-0 focus:ring-transparent !bg-inherit'
-                          style={{
-                            resize: 'none',
-                            overflow: 'auto'
-                          }}
-                          conversationId={undefined}
-                          textareaRef={textareaRef}
-                          autoFocus={true}
-                          onFocus={e => {
-                            const {
-                              target,
-                              target: {
-                                value: { length }
-                              }
-                            } = e;
-                            target.setSelectionRange(length, length);
-                          }}
-                        />
-                      )}
-                      </div>
-                      <div
-                        className={`w-full flex items-center absolute top-3 left-3 space-x-2 circle-padding-on-hover`}
-                      >
-                        {Array(task.pointValue)
-                          .fill()
-                          .map((_, index) => {
-                            console.log(task.id, index);
-                            return (
-                              <span key={index}>
-                                <i class="fa-solid fa-circle font-8 text-offwhite"></i>
-                              </span>
-                            );
-                          })}
-                      </div>
-                      <div
-                        className={`w-full flex items-center justify-between absolute top-3 left-0 ${
-                          !showIcons && 'hidden'
-                        } ${
-                          onlyShowOnHover && !isActive
-                            ? 'invisible group-hover:visible'
-                            : ''
-                        }`}
-                      >
-                        <i
-                          className={`fa-solid fa-clock cursor-pointer text-offwhite w-5 h-5 mr-auto ml-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                          onClick={e => {
-                            e.stopPropagation();
-                            task._editingTimeEstimate =
-                              !task._editingTimeEstimate;
-                          }}
-                        ></i>
-                        <span className='text-gray text-xs absolute text-center top-1 left-1/2 transform -translate-x-1/2'>
-                          {!task._editingTimeEstimate ? (
-                            task.timeEstimate ? (
-                              estimateDisplay(task.timeEstimate)
-                            ) : (
-                              ''
-                            )
-                          ) : (
-                            <input
-                              type='string'
-                              autofocus={true}
-                              onFocus={() => {
-                                this.setSelectionRange(
-                                  this.value.length,
-                                  this.value.length
-                                );
-                              }}
-                              className='w-auto cursor-default text-xs text-center border-1 !important text-sm p-0 m-0 focus:ring-transparent !bg-inherit'
-                              value={estimateDisplay(task.timeEstimate)}
-                              onChange={e => {
-                                e.stopPropagation();
-                                task.timeEstimate = 1;
-                              }}
-                              onKeyDown={e => {
-                                e.stopPropagation();
-                                if (e.key === 'Enter') {
-                                  e.preventDefault();
-                                  setTasks(
-                                    tasks.map(t => {
-                                      if (t.id != task.id) {
-                                        return t;
-                                      }
-                                      return {
-                                        ...t,
-                                        timeEstimate: e.target.value
-                                      };
-                                    })
-                                  );
-                                }
-                              }}
-                              onClick={e => {
-                                e.stopPropagation();
-                              }}
-                            />
-                          )}
-                        </span>
-                        <span className={`items-center text-offwhite w-5 h-5`}>
-                          <i
-                            className={`fa-solid fa-arrow-right cursor-pointer text-offwhite w-5 h-5 absolute transform transition duration-300 hover:scale-125 hover:font-bold`}
-                            onClick={e => {
-                              e.stopPropagation();
-                              client.tasks.postpone.mutate({
-                                taskId: task.id,
-                                projectName: activeProject.name
-                              });
-                            }}
-                          ></i>
-                        </span>
-                        <i
-                          className={`fa-sharp fa-edit cursor-pointer text-offwhite w-5 h-5 ml-auto mr-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                          onClick={e => {
-                            e.stopPropagation();
-                            editingTask?.id != task.id
-                              ? setEditingTask(task)
-                              : setEditingTask(null);
-                          }}
-                        ></i>
-                      </div>
-                      <div
-                        className={`w-full flex items-center justify-between absolute top-1/2 left-0 ${
-                          !showIcons && 'hidden'
-                        } ${
-                          onlyShowOnHover && !isActive
-                            ? 'invisible group-hover:visible'
-                            : ''
-                        }`}
-                      >
-                        <span></span>
-                        <span className='flex items-center justify-center flex-col'>
-                          <i
-                            className={`fa-solid fa-plus cursor-pointer text-offwhite w-5 h-5 ml-auto mr-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                            onClick={e => {
-                              e.stopPropagation();
-                              task.pointValue += 1;
-                              updateTaskMutation.mutate({
-                                id: task.id,
-                                pointValue: task.pointValue
-                              });
-                            }}
-                          ></i>
-                          <i
-                            className={`fa-solid fa-minus cursor-pointer text-offwhite w-5 h-5 mr-auto mr-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                            onClick={e => {
-                              e.stopPropagation();
-                              task.pointValue -= 1;
-                              updateTaskMutation.mutate({
-                                id: task.id,
-                                pointValue: task.pointValue
-                              });
-                            }}
-                          ></i>
-                        </span>
-                        {/* <span></span> */}
-                      </div>
-                      <div
-                        className={`w-full flex items-center justify-between absolute bottom-1 left-0 ${
-                          !showIcons && 'hidden'
-                        } ${
-                          onlyShowOnHover && !isActive
-                            ? 'invisible group-hover:visible'
-                            : ''
-                        }`}
-                      >
-                        <i
-                          className={`fa-solid fa-close cursor-pointer text-offwhite w-5 h-5 mr-auto ml-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                          onClick={e => (
-                            e.stopPropagation(),
-                            api
-                              .closeTask(task.id)
-                              .then(closed => {
-                                setTasks(
-                                  new LinkedList(
-                                    tasks.toArray().filter(t => t.id != task.id)
-                                  )
-                                );
-                                deleteTaskMutation.mutate(task.id);
-                              })
-                              .catch(err => console.log(err))
-                          )}
-                        ></i>
-                        <span className={`items-center text-gray w-5 h-5`}>
-                          <i
-                            className={`fa-solid fa-circle-half-stroke cursor-pointer text-offwhite w-5 h-5 absolute transform transition duration-300 hover:scale-125 hover:font-bold`}
-                            onClick={e => toggleInProgress(task, e)}
-                          ></i>
-                        </span>
-                        <i
-                          className={`fa-solid ${
-                            task.labels.includes('completed')
-                              ? 'fa-undo'
-                              : 'fa-check'
-                          } cursor-pointer text-offwhite w-5 h-5 ml-auto mr-3 transform transition duration-300 hover:scale-125 hover:font-bold`}
-                          onClick={e => toggleCompletion(task, e)}
-                        ></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <TaskBlock
+                  key={task.id + index}
+                  index={index}
+                  task={task}
+                  userInfo={userInfo}
+                  setUserInfo={setUserInfo}
+                  setActiveTask={activateTask}
+                  setActiveTaskIndex={setActiveTaskIndex}
+                  showIcons={showIcons}
+                  isActive={isActive}
+                  someTaskActive={someTaskActive}
+                  pointMap={pointMap}
+                  onlyShowOnHover={onlyShowOnHover}
+                  toggleCompletion={toggleCompletion}
+                  toggleInProgress={toggleInProgress}
+                  activateTask={activateTask}
+                  updateTaskMutation={updateTaskMutation}
+                />
               );
             })}
             <NewTask
@@ -578,6 +330,7 @@ function Tasks({
               setUserInfo={setUserInfo}
               setActiveTask={activateTask}
               setActiveTaskIndex={setActiveTaskIndex}
+              toggleCompletion={toggleCompletion}
             />
           ))
         )}
